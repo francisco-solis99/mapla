4724415c616e1614bc7364fb075c175e4a161b7c
francisco-solis99 <franciscosoliscss@gmail.com>
2023-02-01 09:39:14 -0600

    [Add] the tech-about us page

--- app/Views/pages/technology/index.php
+++ app/Views/pages/technology/index.php
@@ -45,21 +45,54 @@
             <h2 class="text-mapla-purple-200 font-bold font-ubuntu text-[45px]">Pinturas Low Voc</h2>
             <p class="text-mapla-dark-100 max-w-5xl mx-auto">La pintura “Low Voc” se caracteriza por la ausencia del fuerte olor a “ pintura fresca” que desprenden las pinturas que contienen altos niveles de VOC, que incluso pueden causar dolor de cabeza o problemas de memoria cuando se inhalan. Mapla elige hacer pinturas amigables con el medio ambiente y cuidar tu salud, es por eso que en nuestra producción cuidamos minimizar al máximo los Compuestos Volátiles de Carbono.</p>
           </div>
-          <picture class="inline-block max-w-4xl">
+          <picture class="inline-block min-w-[200px] max-w-4xl">
             <source srcset="<?= base_url('/images/about/tech-lowvoc-2x.webp')?>" media="(min-width: 468px)">
             <img src="<?= base_url('/images/about/tech-lowvoc.webp')?>" alt="Pinturas Low Voc" class="w-full object-cover">
           </picture>
         </div>
       </section>
 
-      <section>
-        <div class="container">
-          <article class="text-mapla-dark-100">
-            <h3 class="text-mapla-purple-200 font-ubuntu text-bold">Pinturas antibacteriales 🦠</h3>
-            <p>Mantén tus espacios limpios y seguros de bacterias y microrganismos dañinos para tu salud y la de tu familia, mediante la aplicación de nuestra pintura antibacterial en cualquiera de tus habitaciones.</p>
+      <section class="my-9">
+        <div class="container grid sm:grid-cols-2 lg:grid-cols-3 justify-items-center gap-7">
+          <article class="text-center sm:text-left sm:col-span-2 lg:col-span-1 lg:max-w-sm text-mapla-dark-100">
+            <h3 class="text-mapla-purple-200 font-ubuntu text-bold text-4xl">Pinturas antibacteriales 🦠</h3>
+            <p class="mt-5 text-sm">Mantén tus espacios limpios y seguros de bacterias y microrganismos dañinos para tu salud y la de tu familia, mediante la aplicación de nuestra pintura antibacterial en cualquiera de tus habitaciones.</p>
+          </article>
+          <article class="max-w-sm p-8 bg-mapla-light-200 text-mapla-dark-100 rounded-[20px]">
+            <span class="inline-block bg-white rounded-full p-2 max-w-[50px]">
+              <img src="<?= base_url('/images/about/flower.webp')?>" alt="" class="w-full object-cover">
+            </span>
+            <h3 class="text-mapla-purple-200 text-2xl font-medium mt-3 mb-4">Pinturas sin olor</h3>
+            <p class="text-sm">Nuestras pinturas ecológicas sin olor, a diferencia de la pintura convencional, no contiene partículas invisibles que pueden ser nocivas para tu salud, ya que entre sus componentes se encuentran metales pesados o sustancias volátiles derivadas del petróleo. Además, utilizando este tipo de pintura evitaras tener que esperar varios días para que disminuya el olor a pintura para retomar tus labores cotidianas.</p>
+          </article>
+          <article class="max-w-sm p-8 bg-mapla-light-200 text-mapla-dark-100 rounded-[20px]">
+            <span class="inline-block bg-white rounded-full p-2 max-w-[50px]">
+              <img src="<?= base_url('/images/about/winter.webp')?>" alt="" class="w-full object-cover">
+            </span>
+            <h3 class="text-mapla-purple-200 text-2xl font-medium mt-3 mb-4">Pinturas sin olor</h3>
+            <p class="text-sm">Nuestras pinturas ecológicas sin olor, a diferencia de la pintura convencional, no contiene partículas invisibles que pueden ser nocivas para tu salud, ya que entre sus componentes se encuentran metales pesados o sustancias volátiles derivadas del petróleo. Además, utilizando este tipo de pintura evitaras tener que esperar varios días para que disminuya el olor a pintura para retomar tus labores cotidianas.</p>
           </article>
           <article class="text-mapla-dark-100"></article>
-          <article class="text-mapla-dark-100"></article>
+        </div>
+      </section>
+
+
+      <section class="bg-mapla-purple-200 text-white text-center">
+        <div class="flex flex-col md:grid md:grid-cols-2 ">
+          <div class="p-4 flex flex-col justify-center items-center">
+            <div class="mb-10">
+              <h3 class="py-2 px-3 inline-block justify-self-center text-[28px] bg-mapla-green-100 text-mapla-green-200 font-bold rounded-lg ">#HazloconMapla</h3>
+              <h2 class="mt-10 mb-8 font-bold font-ubuntu text-6xl">Pinturas 100% Mexicanas</h2>
+              <p class="lg:w-4/6 lg:mx-auto mb-4">
+                La mejor calidad con pinturas 100% mexicanas, luce nuestros colores.
+              </p>
+              <a href="<?= url_to('website.promotions.index') ?>" class=" bg-mapla-orange-100 p-3 text-white inline-block rounded-full">Promociones</a>
+            </div>
+          </div>
+          <picture class="inline-block mt-5 md:mt-0">
+            <source srcset="<?= base_url('/images/about/tech-mex-2x.webp')?>" media="(min-width: 768px)">
+            <img src="<?= base_url('/images/about/tech-mex.webp')?>" alt="Soluciones - Pinturas Mapla" class="w-full object-cover h-full">
+          </picture>
         </div>
       </section>
     </main>
