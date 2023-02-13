73445fdfcee408bbfffa02f0bc4297d8a822e75f
francisco-solis99 <franciscosoliscss@gmail.com>
2023-02-13 14:03:51 -0600

    [Fix] padding

--- app/Views/components/Footer.php
+++ app/Views/components/Footer.php
@@ -8,7 +8,7 @@
       <form class="mt-3">
         <label for="newsletter" class="visually__hidden">Correo electronico</label>
         <div class="flex bg-white rounded-full">
-          <input type="text" placeholder="Su correo electronico" id="newsletter" class="w-full py-2 px-4 rounded-tl-full rounded-bl-full">
+          <input type="text" placeholder="Su correo electronico" id="newsletter" class="w-full py-2 px-5 rounded-tl-full rounded-bl-full">
           <button type="submit" class="w-1/2 max-w-[50px] inline-flex items-center justify-center bg-mapla-green-100 rounded-tr-full rounded-br-full">
             <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="none"><path fill="#5600B9" d="M18.6081.3919a1.3281 1.3281 0 0 0-1.2544-.355L2.4327 3.6888A3.184 3.184 0 0 0 0 6.7886v1.9692c0 .7175.5685 1.3046 1.2787 1.3347l5.6125.9554a.371.371 0 0 0 .3247-.1035L13.83 4.3301a.594.594 0 0 1 .8398.8398l-6.6143 6.6143a.371.371 0 0 0-.1034.3246l.9553 5.6126c.03.7101.6172 1.2786 1.3347 1.2786h1.9692a3.184 3.184 0 0 0 3.0999-2.4327l3.6517-14.921.0015-.0062a1.328 1.328 0 0 0-.3564-1.2482Z"/></svg>
           </button>
